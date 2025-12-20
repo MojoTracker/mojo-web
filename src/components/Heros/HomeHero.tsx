7cd1681e8ebc0ab0fe6c3483e94d2babff7c94a2
Zack Yuan <zack6yuan@gmail.com>
2025-12-19 22:07:03 -0600

    added phone image to home screen.

--- src/components/Heros/HomeHero.tsx
+++ src/components/Heros/HomeHero.tsx
@@ -1,10 +1,10 @@
 import ActionButtons from "../ActionButtons";
-import checkmark from "/assets/images/check-check.png";
+import iphone from "/assets/images/iphone.png";
+import diinkerhome from "/assets/images/diinkerhome.png";
 
 export default function HomeHero() {
   return (
     <div className="text-white flex flex-col items-center md:flex-row mt-85 justify-center md:justify-around">
-      {/* 🚀 APPLY text-center HERE or to the h1 directly */}
       <div className="text-center">
         <h1 className="text-5xl md:text-6xl font-nature pb-2">
           Scoring, Simplified.
@@ -14,9 +14,19 @@ export default function HomeHero() {
         </p>
         <ActionButtons />
       </div>
-
-      <div>
-        <img src={checkmark} alt="" className="hidden md:block w-40 h-40" />
+      <div className="relative hidden md:block w-60 h-[440px] lg:w-[300px] lg:h-[550px] transition-all duration-500"> 
+        <img 
+          src={iphone} 
+          alt="iPhone Frame" 
+          className="relative z-10 w-full h-full object-contain pointer-events-none" 
+        />
+        <div className="absolute top-[2%] left-[6.5%] w-[87%] h-[96%] overflow-hidden rounded-[2.5rem] lg:rounded-[3rem] z-0">
+          <img 
+            src={diinkerhome} 
+            alt="App Screenshot" 
+            className="w-full h-[110%] object-cover object-top" 
+          />
+        </div>
       </div>
     </div>
   );
